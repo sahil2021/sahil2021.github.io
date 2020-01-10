087c9ffecfbe4895fd8898fbfbcd4b1823c79162
sahil2021 <48801436+sahil2021@users.noreply.github.com>
2020-01-11 03:57:19 +0530

    Update rel_font.js

--- Envy/rel_font.js
+++ Envy/rel_font.js
@@ -20,39 +20,54 @@ for(var i=0; i<text.length; i++){
     
   }*/
   
+  var tst=document.getElementById("hmdt2");
   var root = document.getElementById("root");
   var list = root.innerHTML;
   var i = 0,j = 0;
-  var id = ["",""];
+  var id = [];
   var temp = "";
-  while(list.charAt(i)!=';'){
+  console.log(list);
+  while(true){
+  //  console.log(list.charAt(i));
     if(list.charAt(i)==','){
+      id[j]= temp; 
       j++;
       i++;
       temp = "";
-      id[j]= temp;
       continue;
     }
+    
+    if(list.charAt(i)==';'){
+      id[j]= temp; 
+      break;
+    }
+    
     temp = temp + list.charAt(i);
     
     i++;
   }
   console.log(id.length);
   
-  var p = document.getElementById("div1_text1");
-  var pp = p.parentElement;
+  var p,pp;
+  
+  for(var k=0;k<id.length;k++){
+  p = document.getElementById(id[k]);
+  console.log(id[k]);
+  pp = p.parentElement;
   var f = window.getComputedStyle(p);
   var ifs = parseInt(f.getPropertyValue('font-size'));
+  console.log(ifs);
   p.style.fontSize = ifs + "px";
   p.style.position = "absolute";
   //p.style.color = "yellow";
   //p.style.width= "50vw";
   //p.style.border ="solid 3px red";
   var text = p.innerHTML;
+  console.log(text);
   var len = text.length;
   //document.body.appendChild(p);
   var s = parseFloat(f.getPropertyValue('width'))/len;
-  console.log(s);
+  console.log(s,len);
   var vw= parseFloat($(pp).width());
   console.log(vw);
   var nw=(0.01*ifs*vw)/len;
@@ -60,6 +75,8 @@ for(var i=0; i<text.length; i++){
   var nf=(ifs/s)*nw;
   p.style.fontSize = nf + "px";
   console.log(nf);
+  p.style.position = "static";
+  }
  
 });
 window.addEventListener('scroll',function(){
