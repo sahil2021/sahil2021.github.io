9b59330e9a5119ae77c3130132ec182034420819
sahil2021 <48801436+sahil2021@users.noreply.github.com>
2020-04-22 22:46:01 +0530

    Update main.js

--- Qaudio/main.js
+++ Qaudio/main.js
@@ -1,6 +1,6 @@
 
 document.getElementById('file_input').addEventListener('change', handleFileSelect, false);
-
+var index = 0;
 var err = document.getElementById("err");
 function handleFileSelect(evt) {
   
@@ -8,27 +8,21 @@ function handleFileSelect(evt) {
     for (var i = 0; i<files.length; i++) {
         var file = files[i];
         var url = file.urn; 
-        try{
+        
         ID3.loadTags(url, function() {
-        showTags(url);
+        showTags(url, URL.createObjectURL(file));
         }, {
         tags: ["title", "artist", "album", "picture"],
         dataReader: ID3.FileAPIReader(file)
         });
       
-    }catch (error) {
-          err.innerHTML = innerHTML+ error;
-    }
-      
     }
   }
   
-function showTags(url) {
+function showTags(url,fileUrl) {
   var tags = ID3.getAllTags(url);
-  err.innerHTML = err.innerHTML+'<br \>'+tags.title;
-  try{
+  //err.innerHTML = err.innerHTML+'<br \>'+tags.title;
   var image = tags.picture;
-  
   var base64;
   if (image) {
     var base64String = "";
@@ -39,31 +33,27 @@ function showTags(url) {
     window.btoa(base64String);
     //err.innerHTML = base64;
   } else {
-    base64 = /*"https://img.icons8.com/office/16/000000/no-image.png"*/"";
+    base64 = "https://img.icons8.com/office/16/000000/no-image.png";
   }
   
   var li = document.createElement("li");
   li.setAttribute("class", "playlist-item");
-  li.setAttribute("id", "playlist_item"+index++);
-  li.setAttribute("data-url",url);
+  li.setAttribute("id", tags.title);
+  li.setAttribute("data-url",fileUrl);
   li.setAttribute("data-artist",tags.artist);
   li.setAttribute("data-album",tags.album);
   li.setAttribute("data-title",tags.title);
   li.setAttribute("onClick","loadSong(this)");
   var img = document.createElement("img");
   img.setAttribute("class", "playlist-item-img");
-  img.setAttribute("id", "playlist_item_img");
   img.setAttribute("src",base64);
   var label = document.createElement("label");
   label.setAttribute("class", "playlist-item-name");
-  label.setAttribute("id", "playlist_item_name"); 
   label.innerHTML= tags.title;
   li.appendChild(img);
   li.appendChild(label);
-  err.innerHTML =li.innerHTML+ err.innerHTML+'<br \>'+ li.getAttribute("data-title");
+  
   document.getElementById('playlist').appendChild(li);
-  } catch (error) {
-    err.innerHTML = innerHTML + error;
-  }
+  
 }
 
