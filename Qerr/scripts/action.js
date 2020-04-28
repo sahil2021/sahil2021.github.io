bf9d603fe3e90b968782844f7d24ed7a1ad8c88e
sahil2021 <48801436+sahil2021@users.noreply.github.com>
2020-04-28 07:06:48 +0530

    Update action.js

--- Qerr/scripts/action.js
+++ Qerr/scripts/action.js
@@ -2,14 +2,18 @@
 function loadSong(li){
   try{
   var c = li.childNodes;
+  var iNX = li.getAttribute("data-index");
+  var e = document.getElementById('err');
+  err.innerHTML = err.innerHTML +"<br />"+ iNX;
+  
   document.getElementById("audio_name").innerHTML= li.getAttribute("data-title") || " ";
   document.getElementById("audio_artist_name").innerHTML= li.getAttribute("data-artist") || " ";
   document.getElementById("audio_thumbnail").setAttribute("src",c[0].getAttribute("src"));
   document.getElementById("audio_title_bar").setAttribute("data-url",li.getAttribute("data-url"));
-  createAudioObj();
-  playPause();
-  var e  = document.getElementById('err');
-  e.innerHTML = e.innerHTML + li.getAttribute("data-url");
+  //err.innerHTML = err.innerHTML +"<br />"+ "ok2";
+  //e.innerHTML = document.getElementById('audio_title_bar').getAttribute("data-url");
+  playAudio(iNX);
+  
   }catch(err){
     document.getElementById("err").innerHTML = err.stack;
   }
