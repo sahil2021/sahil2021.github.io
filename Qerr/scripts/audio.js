4eaf0fb15a15f71574d46f476882c0f5aa7622f7
sahil2021 <48801436+sahil2021@users.noreply.github.com>
2020-05-01 12:54:12 +0530

    Update audio.js

--- Qerr/scripts/audio.js
+++ Qerr/scripts/audio.js
@@ -4,42 +4,44 @@ var er = document.getElementById("err");
 var id = [];
 var howlIndex = -1;
 var previousINX = null;
+var currentINX = null;
 
 function createAudioObj(url){
   
- /*document.getElementById("err").innerHTML = "yep" +"<br />"+ document.getElementById("audio_title_bar").innerHTML+'<br />'+er.innerHTML;*/
-   er.innerHTML = url + "<br />" +er.innerHTML;
    howlPool[++howlIndex] = new Howl({
     src: [url],
-    html5: true,
-    format: "mp3"
+    format: 'mp3',
+    html5: true
   });
-  
+  var tHowl = howlPool[howlIndex];
+  tHowl.on('end',function(){
+    er.innerHTML = "Finished";
+    next();
+  });
 }
 
 function playAudio(iNX){
+  
  try{
    if(previousINX !== null){
      stopAudio(previousINX);
+   }else if(previousINX === null){
+     previousINX = iNX;
+     currentINX = iNX;
    }
     id[iNX] = howlPool[iNX].play();
-    previousINX = iNX;
-}catch(error){
+    er.innerHTML = er.innerHTML + howlPool[iNX].playing(id[iNX]);
+  }catch(error){
    er.innerHTML= er.innerHTML+'<br />'+ error.stack;
- }
+  }
 }
 
 function pauseAudio(iNX){
   howlPool[iNX].pause(id[iNX]);
+  //er.innerHTML = howlPool[iNX].playing(id[iNX]);
 }
 
 function stopAudio(iNX){
   howlPool[iNX].stop(id[iNX]);
-}
-
-function isPlaying(){
-  if(isP === true){
-    return true;
-  }
-  return false;
+  
 }
