bab57caa2b943a3553e77fc6d63bfec0645fac3d
sahil2021 <48801436+sahil2021@users.noreply.github.com>
2020-05-01 12:53:42 +0530

    Update action.js

--- Qerr/scripts/action.js
+++ Qerr/scripts/action.js
@@ -3,18 +3,44 @@ function loadSong(li){
   try{
   var c = li.childNodes;
   var iNX = li.getAttribute("data-index");
-  var e = document.getElementById('err');
-  err.innerHTML = err.innerHTML +"<br />"+ iNX;
-  
   document.getElementById("audio_name").innerHTML= li.getAttribute("data-title") || " ";
   document.getElementById("audio_artist_name").innerHTML= li.getAttribute("data-artist") || " ";
   document.getElementById("audio_thumbnail").setAttribute("src",c[0].getAttribute("src"));
   document.getElementById("audio_title_bar").setAttribute("data-url",li.getAttribute("data-url"));
-  //err.innerHTML = err.innerHTML +"<br />"+ "ok2";
-  //e.innerHTML = document.getElementById('audio_title_bar').getAttribute("data-url");
+  
   playAudio(iNX);
   
   }catch(err){
     document.getElementById("err").innerHTML = err.stack;
   }
+}
+
+function playPause(){
+  document.getElementById("err").innerHTML = howlPool[currentINX].playing(id[currentINX]);
+  if(howlPool[currentINX].playing(id[currentINX])){
+    pauseAudio(currentINX);
+  }else{
+    playAudio(currentINX);
+  }
+}
+
+function next(){
+  er.innerHTML = er.innerHTML + currentINX +" "+ howlPool.length;
+  if (currentINX == ( howlPool.length - 1)) {
+    playAudio(0);
+  } else {
+    playAudio(++currentINX);
+  }
+}
+
+function previous(){
+  
+}
+
+function shuffle(){
+  
+}
+
+function repeat(){
+  
 }
