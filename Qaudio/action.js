59ac3eba31a625b6cbda73e6779ee08a7a54281a
sahil2021 <48801436+sahil2021@users.noreply.github.com>
2020-05-04 15:20:36 +0530

    Delete action.js

--- Qaudio/action.js
+++
@@ -1,6 +0,0 @@
-function loadSong(li){
-  var c = li.childNodes;
-  document.getElementById("audio_name").innerHTML= '<a href="'+li.getAttribute("data-url")+'" target="_blank">' +li.getAttribute("data-title")+'</a>';       
-  document.getElementById("audio_artist_name").innerHTML= li.getAttribute("data-artist") || " ";
-  document.getElementById("audio_thumbnail").setAttribute("src",c[0].getAttribute("src"));
-}
