0f1e245b4970cddeec27e5a16a7dcd178e2dee79
sahil2021 <48801436+sahil2021@users.noreply.github.com>
2020-05-07 05:06:41 +0530

    Update audio.js

--- Qaudio/scripts/audio.js
+++ Qaudio/scripts/audio.js
@@ -1,6 +1,5 @@
 var howlPool = [];
 var isP= false;
-var er = document.getElementById("err");
 var id = [];
 var howlIndex = -1;
 var previousINX = -1;
@@ -8,6 +7,7 @@ var currentINX = -1;
 var nextINX = -1;
 var previousPlaying = -1;
 var intvId;
+
 function createAudioObj(url){
   
    howlPool[++howlIndex] = new Howl({
@@ -15,21 +15,32 @@ function createAudioObj(url){
     format: 'mp3',
     html5: true
   });
+  
+  
   var tHowl = howlPool[howlIndex];
   tHowl.on('end',function(){
+    clearInterval(invtId);
     if(repeatState == false){
-      next();
+      er.innerHTML = er.innerHTML + 'ok';
+       next();
     }else{
       id[currentINX] = howlPool[currentINX].play();
     }
+  });
+  
+  
+  tHowl.on('pause',function(){
+    
+    document.getElementById("play_pause").src = "icons/play.svg";
+    
     clearInterval(invtId);
   });
-  tHowl.on('pause',function(){
-    document.getElementById("play_pause").src = "icons/play.svg";
-    clearInterval(invtId)
-  });
+  
+  
   tHowl.on('play',function(){
+    
     document.getElementById("play_pause").src = "icons/pause.svg";
+    
     var duration = parseInt(howlPool[currentINX].duration(id[currentINX]));
     seekBar.max = duration;
     invtId = setInterval(seekPlayback,1000);
@@ -37,74 +48,76 @@ function createAudioObj(url){
   })
 }
 
-function playAudio(iNX,dir){
+function playAudio(iNX){
   
  try{
-   if(currentINX != -1 && iNX != 0 && iNX != howlPool.length-1){
-     previousINX = iNX - 1;
-     currentINX = iNX;
-     nextINX = parseInt(currentINX)+1;
-     //er.innerHTML = er.innerHTML + " "+nextINX+" 12 "+currentINX+" ";
-     if(dir == 0){
-       stopAudio(previousINX);
-     }else if(dir == 1){
-       stopAudio(nextINX);
-     }else{
-       stopAudio(previousPlaying);
-     }
-   }else if(currentINX == -1){
-     currentINX = iNX;
-     if(currentINX == 0){
-      //er.innerHTML+= "ok";
-         previousINX = howlPool.length - 1;
-     }else{
-       previousINX = iNX-1;
-     }
-     if(currentINX == howlPool.length-1){
-       nextINX = 0;
-     }else{
-       nextINX = parseInt(currentINX)+1;
-     }
-     //er.innerHTML = er.innerHTML + previousINX+" 2 "+currentINX +" 1 "+nextINX;
-   }else if(iNX == 0){
-    //er.innerHTML = er.innerHTML + previousINX+" "+currentINX+' '+nextINX+'</br>'';
-     previousINX = howlPool.length - 1;
-     currentINX = iNX;
-     nextINX = 1;
-     if (dir == 0) {
-       stopAudio(previousINX);
-     } else if(dir == 1){
-       stopAudio(nextINX);
-     } else{
-       stopAudio(previousPlaying);
-     }
+   changeStates(iNX);
+   
+   if(previousPlaying == -1){
+     // Nothing to Stop
    }else{
-     previousINX = iNX - 1;
-     currentINX = iNX;
-     nextINX = 0;
-     //er.innerHTML = er.innerHTML + " " + nextINX + " 12 " + currentINX + " ";
-     if (dir == 0) {
-       stopAudio(previousINX);
-     } else if(dir == 1){
-       stopAudio(nextINX);
-     } else {
-        stopAudio(previousPlaying);
-     }
+     stopAudio(previousPlaying);
    }
-   //er.innerHTML = er.innerHTML + previousINX+" 4 "+currentINX+" 5 "+nextINX;
-    id[iNX] = howlPool[iNX].play();
-    previousPlaying = iNX;
+   
+   id[iNX] = howlPool[iNX].play();
+   previousPlaying = iNX;
     
   }catch(error){
    er.innerHTML= er.innerHTML+'<br />'+ error.stack;
   }
 }
 
+
 function pauseAudio(iNX){
   howlPool[iNX].pause(id[iNX]);
-  //er.innerHTML = howlPool[iNX].playing(id[iNX]);
 }
+
 
 function stopAudio(iNX){
   howlPool[iNX].stop(id[iNX]);
+}
+
+
+
+function changeStates(iNX){
+  if (currentINX != -1 && iNX != 0 && iNX != howlPool.length - 1) {
+    
+    previousINX = iNX - 1;
+    currentINX = iNX;
+    nextINX = parseInt(currentINX) + 1;
+    er.innerHTML = er.innerHTML +"<br />1 "+ previousINX+" "+currentINX+" "+nextINX;
+  } else if(currentINX == -1) {
+    
+    currentINX = iNX;
+    
+    if(currentINX == 0) {
+      previousINX = howlPool.length - 1;
+    } else{
+      previousINX = iNX - 1;
+    }
+    
+    if(currentINX == howlPool.length - 1) {
+      nextINX = 0;
+    } else{
+      nextINX = parseInt(currentINX) + 1;
+    }
+    er.innerHTML = er.innerHTML +"<br />2 "+ previousINX+" "+currentINX+" "+nextINX;
+    
+  } else if(iNX == 0) {
+    
+    previousINX = howlPool.length - 1;
+    currentINX = iNX;
+    nextINX = 1;
+    
+    er.innerHTML = er.innerHTML +"<br />3 "+ previousINX+" "+currentINX+" "+nextINX;
+    
+  } else{
+    
+    previousINX = iNX - 1;
+    currentINX = iNX;
+    nextINX = 0;
+    
+    er.innerHTML = er.innerHTML +"<br />4"+ previousINX+" "+currentINX+" "+nextINX;
+  }
+  
 }
